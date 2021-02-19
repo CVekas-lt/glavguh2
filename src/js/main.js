1ff493bfd8d2bba693b8faa04585dfcfc1a3c2e4
sanyokstd <59456430+sanyokstd@users.noreply.github.com>
2021-02-19 12:28:41 +0200

    1
    1

--- src/js/main.js
+++ src/js/main.js
@@ -6,4 +6,21 @@
         burger.classList.toggle("active");
         sidebar.classList.toggle("active");
     }
+
+
+
 })();
+
+
+function addVal() {
+    let infoLabel = document.querySelector('#add-info')
+    infoLabel.style.display = "block";
+    infoLabel.classList.add('fadein')
+    setTimeout(function() {
+        infoLabel.classList.remove('fadein')
+        setTimeout(function() {
+            infoLabel.style.display = "none";
+        }, 300)
+
+    }, 2000)
+}
